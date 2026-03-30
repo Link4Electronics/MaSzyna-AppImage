4e19881964c1f6bd05aa2a90f6d074cffc24370c
Link4Electronics <link4electronics@gmail.com>
2026-03-30 18:30:53 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,30 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    asio           \
+    cmake          \
+    glfw           \
+    glm            \
+    libserialport  \
+    luajit         \
+    openal         \
+    openvr         \
+    vulkan-headers
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building MaSzyna..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/MaSzyna-EU07/maszyna"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive "$REPO" ./maszyna
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+#export CXXFLAGS="$CXXFLAGS -Wno-error=format-security"
+cmake -S ./maszyna -B build -DCMAKE_BUILD_TYPE=Release -DWITH_BETTER_RENDERER=OFF -DWITH_DISCORD_RPC=OFF -DWITH_OPENVR=ON
+cmake --build build -j$(nproc)
+mv -v build/bin/eu07* ./AppDir/bin/maszyna

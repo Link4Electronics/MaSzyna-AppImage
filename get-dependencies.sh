1230f81ca13f4a762318f81a8a43254d9e8b157e
Link4Electronics <link4electronics@gmail.com>
2026-03-30 19:03:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,10 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cmake         \
+    libserialport \
+    openvr
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"

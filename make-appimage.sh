ea70eaa5b22ba7d28aaa99df6dab270778f12266
Link4Electronics <link4electronics@gmail.com>
2026-07-03 10:23:23 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q maszyna-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=DUMMY
-export MAIN_BIN=eu07
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/maszyna ./AppDir/bin/eu07
+quick-sharun ./AppDir/bin/maszyna
 
 # Additional changes can be done in between here
 
